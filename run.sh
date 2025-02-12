ee55be9b2ff6d28c34d1b35bd921ca155141d8e8
Haustuer <54508934+Haustuer@users.noreply.github.com>
2025-02-11 18:10:38 -0800

    Create run.sh
    lets see

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Shell script to pull the latest changes from a Git repository
+
+# Navigate to the repository directory (replace with your actual path)
+# cd /git/ePaper
+
+# Run the git pull command with sudo
+sudo git pull
+sudo make clean
+sudo make -j4
+sudo ./epd 1 2
