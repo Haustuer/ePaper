1ca3f812bf8232a979935958d415699ace689ba8
Haustuer <54508934+Haustuer@users.noreply.github.com>
2025-02-12 02:00:28 -0800

    unicircle

--- node/backend.js
+++ node/backend.js
@@ -78,7 +78,7 @@ app.get('/', (req, res) => {
 
   var ShipList = ["211238300", "211735050", "211713930", "353136000", "368207620", "367719770", "211476060", "228131430", "211222710"];
 // Constants
-const R = 6371.0; // Radius of the Earth in kilometers
+const R = 1; // Radius of the Earth in kilometers
 // Constants
 const WIDTH = 1872;  // Width of the map in pixels
 const HEIGHT = 1404; // Height of the map in pixels
