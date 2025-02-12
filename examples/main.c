fae2de76cb462c087f35c14dbffef6be664cbaed
Haustuer <54508934+Haustuer@users.noreply.github.com>
2025-02-11 20:32:35 -0800

    Array

--- examples/main.c
+++ examples/main.c
@@ -13,8 +13,6 @@
 #include <arpa/inet.h> //inet_addr
 /* */
 
-
-
 UWORD VCOM = 1520;
 IT8951_Dev_Info Dev_Info = {0, 0};
 UWORD Panel_Width;
@@ -95,7 +93,6 @@ int main(int argc, char *argv[])
 
     Debug("Arg 1:%u\r\n", temp);
     Debug("Arg 2:%u\r\n", temp1);
-    
 
     // Init the BCM2835 Device
     if (DEV_Module_Init() != 0)
@@ -131,59 +128,72 @@ int main(int argc, char *argv[])
     case 3:
         /* code */
 
-
-
         Debug("Start of Socket");
 
         int socket_desc;
         struct sockaddr_in server;
-        char *message , server_reply[2000];
-  
-        //Create socket
-        socket_desc = socket(AF_INET , SOCK_STREAM , 0);
-        if (socket_desc == -1) Debug("Could not create socket");
-  
+        char *message, server_reply[2000];
+
+        // Create socket
+        socket_desc = socket(AF_INET, SOCK_STREAM, 0);
+        if (socket_desc == -1)
+            Debug("Could not create socket");
+
         server.sin_addr.s_addr = inet_addr("127.0.0.1");
         server.sin_family = AF_INET;
-        server.sin_port = htons( 10443 );
-  
-        //Connect to remote server
-        if (connect(socket_desc , (struct sockaddr *)&server , sizeof(server)) < 0){
+        server.sin_port = htons(10443);
+
+        // Connect to remote server
+        if (connect(socket_desc, (struct sockaddr *)&server, sizeof(server)) < 0)
+        {
             puts("connect error");
             return 1;
         }
-  
+
         puts("Connected\n");
-  
-        //Send some data
-     //   message = "GET / HTTP/1.1\r\n\r\n";// copilot
-     
+
+        // Send some data
+        //   message = "GET / HTTP/1.1\r\n\r\n";// copilot
+
         message = "GET /sprites HTTP/1.1\r\nHost: 127.0.0.1\r\n\r\n";
-        if( send(socket_desc , message , strlen(message) , 0) < 0){
+        if (send(socket_desc, message, strlen(message), 0) < 0)
+        {
             puts("Send failed");
             return 1;
         }
         puts("Data Send\n");
-  
-        //Receive a reply from the server
-        if (recv(socket_desc, server_reply , 2000 , 0) < 0){
+
+        // Receive a reply from the server
+        int received_length = recv(socket_desc, server_reply, 2000, 0);
+        if (received_length < 0)
+        {
             puts("recv failed");
+            return 1;
         }
+        server_reply[received_length] = '\0'; // Null-terminate the server reply
         puts("Reply received\n");
         puts(server_reply);
-  
-  
+
+        // Find the start of the JSON payload
+        char *json_start = strstr(server_reply, "\r\n\r\n");
+        if (json_start == nullptr)
+        {
+            puts("Could not find JSON payload");
+            return 1;
+        }
+        json_start += 4; // Skip the "\r\n\r\n"
+
+        // Parse the JSON payload
+        std::string json_payload(json_start);
+        auto json_data = nlohmann::json::parse(json_payload);
+
+        // Print the extracted JSON data
+        for (const auto &item : json_data)
+        {
+            printf("x: %d, y: %d, icon: %d\n", item["x"], item["y"], item["icon"]);
+        }
+
         Debug("end of Socket");
-
-
-
-
-
-
-
-
-
-
 
         break;
 
@@ -192,9 +202,6 @@ int main(int argc, char *argv[])
 
         break;
     }
-
-    
-  
 
     /* //Show A2 mode refresh effect
      EPD_IT8951_Clear_Refresh(Dev_Info, Init_Target_Memory_Addr, A2_Mode);
