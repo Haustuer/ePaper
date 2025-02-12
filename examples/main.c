5ce32bd38c97691f7ce5b50a9ca9182e206cc211
Haustuer <54508934+Haustuer@users.noreply.github.com>
2025-02-11 19:48:38 -0800

    REST API

--- examples/main.c
+++ examples/main.c
@@ -145,7 +145,7 @@ int main(int argc, char *argv[])
   
         server.sin_addr.s_addr = inet_addr("127.0.0.1");
         server.sin_family = AF_INET;
-        server.sin_port = htons( 80 );
+        server.sin_port = htons( 10443 );
   
         //Connect to remote server
         if (connect(socket_desc , (struct sockaddr *)&server , sizeof(server)) < 0){
@@ -157,7 +157,8 @@ int main(int argc, char *argv[])
   
         //Send some data
      //   message = "GET / HTTP/1.1\r\n\r\n";// copilot
-        message = "GET / HTTP/1.1\r\nHost: 127.0.0.1\r\n\r\n";
+     
+        message = "GET /ships HTTP/1.1\r\nHost: 127.0.0.1\r\n\r\n";
         if( send(socket_desc , message , strlen(message) , 0) < 0){
             puts("Send failed");
             return 1;
