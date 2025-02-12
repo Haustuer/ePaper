563863cea9d36ce1212dd86c3afc9d4465cad4c8
Haustuer <54508934+Haustuer@users.noreply.github.com>
2025-02-11 19:18:28 -0800

    socket

--- examples/main.c
+++ examples/main.c
@@ -4,17 +4,17 @@
 
 #include <math.h>
 
-#include <stdlib.h>     //exit()
+#include <stdlib.h> //exit()
 /*  TEst with new libs*/
-#include <signal.h>     //signal()
+#include <signal.h> //signal()
 #include <stdio.h>
-#include <string.h>	//strlen
+#include <string.h> //strlen
 #include <sys/socket.h>
-#include <arpa/inet.h>	//inet_addr
+#include <arpa/inet.h> //inet_addr
 /* */
 #include <curl/curl.h>
 
-/* 
+/*
 struct string {
     char *ptr;
     size_t len;
@@ -54,250 +54,226 @@ IT8951_Dev_Info Dev_Info = {0, 0};
 UWORD Panel_Width;
 UWORD Panel_Height;
 UDOUBLE Init_Target_Memory_Addr;
-int epd_mode = 1;	//1: no rotate, horizontal mirror, for 10.3inch
-					
-void  Handler(int signo){
+int epd_mode = 1; // 1: no rotate, horizontal mirror, for 10.3inch
+
+void Handler(int signo)
+{
     Debug("\r\nHandler:exit\r\n");
-    if(Refresh_Frame_Buf != NULL){
+    if (Refresh_Frame_Buf != NULL)
+    {
         free(Refresh_Frame_Buf);
         Debug("free Refresh_Frame_Buf\r\n");
         Refresh_Frame_Buf = NULL;
     }
-    if(Panel_Frame_Buf != NULL){
+    if (Panel_Frame_Buf != NULL)
+    {
         free(Panel_Frame_Buf);
         Debug("free Panel_Frame_Buf\r\n");
         Panel_Frame_Buf = NULL;
     }
-    if(Panel_Area_Frame_Buf != NULL){
+    if (Panel_Area_Frame_Buf != NULL)
+    {
         free(Panel_Area_Frame_Buf);
         Debug("free Panel_Area_Frame_Buf\r\n");
         Panel_Area_Frame_Buf = NULL;
     }
-    if(bmp_src_buf != NULL){
+    if (bmp_src_buf != NULL)
+    {
         free(bmp_src_buf);
         Debug("free bmp_src_buf\r\n");
         bmp_src_buf = NULL;
     }
-    if(bmp_dst_buf != NULL){
+    if (bmp_dst_buf != NULL)
+    {
         free(bmp_dst_buf);
         Debug("free bmp_dst_buf\r\n");
         bmp_dst_buf = NULL;
     }
-	if(Dev_Info.Panel_W != 0){
-		Debug("Going to sleep\r\n");
-		EPD_IT8951_Sleep();
-	}
+    if (Dev_Info.Panel_W != 0)
+    {
+        Debug("Going to sleep\r\n");
+        EPD_IT8951_Sleep();
+    }
     DEV_Module_Exit();
     exit(0);
 }
 
-
 int main(int argc, char *argv[])
 {
-    //Exception handling:ctrl + c
-    signal(SIGINT, Handler); 
+    // Exception handling:ctrl + c
+    signal(SIGINT, Handler);
 
-
-
-
-
-
- if (argc < 2){
+    if (argc < 2)
+    {
         Debug("Not 2 Args!\r\n");
-       
-    
-    }if (argc != 3){
-		Debug("nor 3 Args!\r\n");
-		
+    }
+    if (argc != 3)
+    {
+        Debug("nor 3 Args!\r\n");
     }
 
-      
-    int temp; 
+    int temp;
+    int temp1;
 
-    int  temp1;
+    if (argc == 3)
+    {
 
-    if (argc == 3){
-        
-        sscanf(argv[1],"%u",&temp);      
-
-        sscanf(argv[2],"%u",&temp1);
-      
-    }else{
-        temp=1;
-        temp1=0;
+        sscanf(argv[1], "%u", &temp);
+        sscanf(argv[2], "%u", &temp1);
     }
-
+    else
+    {
+        temp = 1;
+        temp1 = 0;
+    }
 
     Debug("Arg 1:%u\r\n", temp);
     Debug("Arg 2:%u\r\n", temp1);
     
-    //Init the BCM2835 Device
-    if(DEV_Module_Init()!=0){
+
+    // Init the BCM2835 Device
+    if (DEV_Module_Init() != 0)
+    {
         return -1;
     }
-   
-	
-
-    
-
-    //Init the BCM2835 Device
-    if(DEV_Module_Init()!=0){
-        return -1;
-    }    
-    Debug("VCOM value:%d\r\n", VCOM);	
+    Debug("VCOM value:%d\r\n", VCOM);
     Debug("Display mode:%d\r\n", epd_mode);
     Dev_Info = EPD_IT8951_Init(VCOM);
 
-    //get some important info from Dev_Info structure
+    // get some important info from Dev_Info structure
     Panel_Width = Dev_Info.Panel_W;
     Panel_Height = Dev_Info.Panel_H;
     Init_Target_Memory_Addr = Dev_Info.Memory_Addr_L | (Dev_Info.Memory_Addr_H << 16);
-    A2_Mode = 6;    
+    A2_Mode = 6;
     Debug("A2 Mode:%d\r\n", A2_Mode);
-
-
-
 
     switch (temp)
     {
     case 1:
-    //Show a bmp file
-    //1bp use A2 mode by default, before used it, refresh the screen with WHITE
-    //Display_BMP_Example(Panel_Width, Panel_Height, Init_Target_Memory_Addr, BitsPerPixel_1);
-    //Display_BMP_Example(Panel_Width, Panel_Height, Init_Target_Memory_Addr, BitsPerPixel_2);
-    Debug(" Show Pic");
-    Display_BMP_Example(Panel_Width, Panel_Height, Init_Target_Memory_Addr, BitsPerPixel_4);
+        // Show a bmp file
+        // 1bp use A2 mode by default, before used it, refresh the screen with WHITE
+        // Display_BMP_Example(Panel_Width, Panel_Height, Init_Target_Memory_Addr, BitsPerPixel_1);
+        // Display_BMP_Example(Panel_Width, Panel_Height, Init_Target_Memory_Addr, BitsPerPixel_2);
+        Debug(" Show Pic");
+        Display_BMP_Example(Panel_Width, Panel_Height, Init_Target_Memory_Addr, BitsPerPixel_4);
         break;
-        case 2:
+    case 2:
         Debug(" Clear Pic");
         EPD_IT8951_Clear_Refresh(Dev_Info, Init_Target_Memory_Addr, GC16_Mode);
         /* code */
         break;
-        case 3:
+    case 3:
         /* code */
         break;
-    
-    default:
-    EPD_IT8951_Clear_Refresh(Dev_Info, Init_Target_Memory_Addr, INIT_Mode);
 
+    default:
+        EPD_IT8951_Clear_Refresh(Dev_Info, Init_Target_Memory_Addr, INIT_Mode);
 
         break;
     }
 
-
-    
- 
-	
-
-/*
+    /*
 
 
-    // Initialize the curl library
-  curl_global_init(CURL_GLOBAL_DEFAULT);
-  // Create a curl handle
-  CURL *handle = curl_easy_init();
-  // Set the URL to request
-  curl_easy_setopt(handle, CURLOPT_URL, "http://127.0.0.1");
-  // Set the callback function to handle the response
-  curl_easy_setopt(handle, CURLOPT_WRITEFUNCTION, [](char *data, size_t size, size_t nmemb, void *userp) {
-    // Cast the user pointer to an ostream and write the data to it
-    *static_cast<std::ostream*>(userp) << data;
-    // Return the number of bytes processed
-    return size * nmemb;
-  });
-  // Set the user pointer to be an ostream to which the response will be written
-  std::ostringstream response;
-  curl_easy_setopt(handle, CURLOPT_WRITEDATA, &response);
-  // Perform the request
-  CURLcode result = curl_easy_perform(handle);
-  // Check the result
-  if (result != CURLE_OK) {
-    // If the request failed, print an error message
-    std::cerr << "curl_easy_perform() failed: " << curl_easy_strerror(result) << std::endl;
-  } else {
-    // If the request was successful, print the response
-    std::cout << response.str() << std::endl;
-  }
-  // Clean up
-  curl_easy_cleanup(handle);
-  curl_global_cleanup();
+        // Initialize the curl library
+      curl_global_init(CURL_GLOBAL_DEFAULT);
+      // Create a curl handle
+      CURL *handle = curl_easy_init();
+      // Set the URL to request
+      curl_easy_setopt(handle, CURLOPT_URL, "http://127.0.0.1");
+      // Set the callback function to handle the response
+      curl_easy_setopt(handle, CURLOPT_WRITEFUNCTION, [](char *data, size_t size, size_t nmemb, void *userp) {
+        // Cast the user pointer to an ostream and write the data to it
+        *static_cast<std::ostream*>(userp) << data;
+        // Return the number of bytes processed
+        return size * nmemb;
+      });
+      // Set the user pointer to be an ostream to which the response will be written
+      std::ostringstream response;
+      curl_easy_setopt(handle, CURLOPT_WRITEDATA, &response);
+      // Perform the request
+      CURLcode result = curl_easy_perform(handle);
+      // Check the result
+      if (result != CURLE_OK) {
+        // If the request failed, print an error message
+        std::cerr << "curl_easy_perform() failed: " << curl_easy_strerror(result) << std::endl;
+      } else {
+        // If the request was successful, print the response
+        std::cout << response.str() << std::endl;
+      }
+      // Clean up
+      curl_easy_cleanup(handle);
+      curl_global_cleanup();
 
 
-*/
+    */
 
+    /*
 
+      int socket_desc;
+      struct sockaddr_in server;
+      char *message , server_reply[2000];
 
- 
-   
+      //Create socket
+      socket_desc = socket(AF_INET , SOCK_STREAM , 0);
+      if (socket_desc == -1) Debug("Could not create socket");
 
+      server.sin_addr.s_addr = inet_addr("127.0.0.1");
+      server.sin_family = AF_INET;
+      server.sin_port = htons( 80 );
 
-  /*
+      //Connect to remote server
+      if (connect(socket_desc , (struct sockaddr *)&server , sizeof(server)) < 0){
+          puts("connect error");
+          return 1;
+      }
 
-    int socket_desc;
-	struct sockaddr_in server;
-	char *message , server_reply[2000];
-	
-	//Create socket
-	socket_desc = socket(AF_INET , SOCK_STREAM , 0);
-	if (socket_desc == -1) Debug("Could not create socket");
-		
-	server.sin_addr.s_addr = inet_addr("127.0.0.1");
-	server.sin_family = AF_INET;
-	server.sin_port = htons( 80 );
+      puts("Connected\n");
 
-	//Connect to remote server
-	if (connect(socket_desc , (struct sockaddr *)&server , sizeof(server)) < 0){
-		puts("connect error");
-		return 1;
-	}
-	
-	puts("Connected\n");
-	
-	//Send some data
-	message = "GET / HTTP/1.1\r\n\r\n";
-	if( send(socket_desc , message , strlen(message) , 0) < 0){
-		puts("Send failed");
-		return 1;
-	}
-	puts("Data Send\n");
-	
-	//Receive a reply from the server
-	if (recv(socket_desc, server_reply , 2000 , 0) < 0){
-		puts("recv failed");
-	}
-	puts("Reply received\n");
-	puts(server_reply);
-    
-*/
-    
-   /* //Show A2 mode refresh effect
-    EPD_IT8951_Clear_Refresh(Dev_Info, Init_Target_Memory_Addr, A2_Mode);
-    Dynamic_Refresh_Example(Dev_Info,Init_Target_Memory_Addr);
-    EPD_IT8951_Clear_Refresh(Dev_Info, Init_Target_Memory_Addr, A2_Mode);
-    EPD_IT8951_Clear_Refresh(Dev_Info, Init_Target_Memory_Addr, GC16_Mode);
-	
-    //Show how to display a gif, not works well on 6inch e-Paper HAT, 9.7inch e-Paper HAT, others work well
-    EPD_IT8951_Clear_Refresh(Dev_Info, Init_Target_Memory_Addr, A2_Mode);
-    Dynamic_GIF_Example(Panel_Width, Panel_Height, Init_Target_Memory_Addr);
-    EPD_IT8951_Clear_Refresh(Dev_Info, Init_Target_Memory_Addr, A2_Mode);
-    EPD_IT8951_Clear_Refresh(Dev_Info, Init_Target_Memory_Addr, GC16_Mode);
+      //Send some data
+      message = "GET / HTTP/1.1\r\n\r\n";
+      if( send(socket_desc , message , strlen(message) , 0) < 0){
+          puts("Send failed");
+          return 1;
+      }
+      puts("Data Send\n");
 
-    //Show how to test frame rate, test it individually,which is related to refresh area size and refresh mode
-    EPD_IT8951_Clear_Refresh(Dev_Info, Init_Target_Memory_Addr, A2_Mode);
-    Check_FrameRate_Example(800, 600, Init_Target_Memory_Addr, BitsPerPixel_1);
-    EPD_IT8951_Clear_Refresh(Dev_Info, Init_Target_Memory_Addr, A2_Mode);
-    EPD_IT8951_Clear_Refresh(Dev_Info, Init_Target_Memory_Addr, GC16_Mode);*/
+      //Receive a reply from the server
+      if (recv(socket_desc, server_reply , 2000 , 0) < 0){
+          puts("recv failed");
+      }
+      puts("Reply received\n");
+      puts(server_reply);
 
+  */
 
-    //We recommended refresh the panel to white color before storing in the warehouse.
-//    EPD_IT8951_Clear_Refresh(Dev_Info, Init_Target_Memory_Addr, INIT_Mode);
+    /* //Show A2 mode refresh effect
+     EPD_IT8951_Clear_Refresh(Dev_Info, Init_Target_Memory_Addr, A2_Mode);
+     Dynamic_Refresh_Example(Dev_Info,Init_Target_Memory_Addr);
+     EPD_IT8951_Clear_Refresh(Dev_Info, Init_Target_Memory_Addr, A2_Mode);
+     EPD_IT8951_Clear_Refresh(Dev_Info, Init_Target_Memory_Addr, GC16_Mode);
 
-    //EPD_IT8951_Standby();
-//   EPD_IT8951_Sleep();
+     //Show how to display a gif, not works well on 6inch e-Paper HAT, 9.7inch e-Paper HAT, others work well
+     EPD_IT8951_Clear_Refresh(Dev_Info, Init_Target_Memory_Addr, A2_Mode);
+     Dynamic_GIF_Example(Panel_Width, Panel_Height, Init_Target_Memory_Addr);
+     EPD_IT8951_Clear_Refresh(Dev_Info, Init_Target_Memory_Addr, A2_Mode);
+     EPD_IT8951_Clear_Refresh(Dev_Info, Init_Target_Memory_Addr, GC16_Mode);
 
-    //In case RPI is transmitting image in no hold mode, which requires at most 10s
-//    DEV_Delay_ms(5000);
+     //Show how to test frame rate, test it individually,which is related to refresh area size and refresh mode
+     EPD_IT8951_Clear_Refresh(Dev_Info, Init_Target_Memory_Addr, A2_Mode);
+     Check_FrameRate_Example(800, 600, Init_Target_Memory_Addr, BitsPerPixel_1);
+     EPD_IT8951_Clear_Refresh(Dev_Info, Init_Target_Memory_Addr, A2_Mode);
+     EPD_IT8951_Clear_Refresh(Dev_Info, Init_Target_Memory_Addr, GC16_Mode);*/
 
-//    DEV_Module_Exit();
+    // We recommended refresh the panel to white color before storing in the warehouse.
+    //    EPD_IT8951_Clear_Refresh(Dev_Info, Init_Target_Memory_Addr, INIT_Mode);
+
+    // EPD_IT8951_Standby();
+    //   EPD_IT8951_Sleep();
+
+    // In case RPI is transmitting image in no hold mode, which requires at most 10s
+    //    DEV_Delay_ms(5000);
+
+    //    DEV_Module_Exit();
     return 0;
 }
