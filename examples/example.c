a1489ea36fcca462337b296d45942e57c0e50fab
Haustuer <54508934+Haustuer@users.noreply.github.com>
2025-02-16 19:42:21 -0800

    Update example.c

--- examples/example.c
+++ examples/example.c
@@ -345,6 +345,8 @@ parameter:
     Init_Target_Memory_Addr: Memory address of IT8951 target memory address
     BitsPerPixel: Bits Per Pixel, 2^BitsPerPixel = grayscale
 ******************************************************************************/
+
+
 UBYTE Display_BMP_HST1(UWORD Panel_Width, UWORD Panel_Height, UDOUBLE Init_Target_Memory_Addr, UBYTE BitsPerPixel, int x, int y)
 {
     UWORD WIDTH;
@@ -429,17 +431,58 @@ parameter:
     Init_Target_Memory_Addr: Memory address of IT8951 target memory address
     BitsPerPixel: Bits Per Pixel, 2^BitsPerPixel = grayscale
 ******************************************************************************/
-UBYTE Display_BMP_HST(UWORD Panel_Width, UWORD Panel_Height, UDOUBLE Init_Target_Memory_Addr, UBYTE BitsPerPixel, int x, int y)
+
+UBYTE Display_BMP_Shor2( UDOUBLE Init_Target_Memory_Addr ,int x, int y,int icon)
 {
     UWORD WIDTH;
+
+    UBYTE BitsPerPixel = BitsPerPixel_8;
+ 
+    UWORD Panel_Width =  114;
+    UWORD Panel_Height = 138;
+    char Path[30];
+
+    switch (icon)
+    {
+        case 1:
+    Panel_Width =  159;
+    Panel_Height = 101;
+    sprintf(Path, "./pic/2.bmp");
+    break;
+
+    case 2:
+    Panel_Width =  114;
+    Panel_Height = 138;
+    sprintf(Path, "./pic/2.bmp");
+
+        /* code */
+        break;
+
+        case 3:
+    Panel_Width =  182;
+    Panel_Height = 58;
+    sprintf(Path, "./pic/2.bmp");
+    break;
+
+    default:
+    Panel_Width =  114;
+    Panel_Height = 138;
+    sprintf(Path, "./pic/2.bmp");
+        break;
+    }
+
+
+
 
     if (Four_Byte_Align == true)
     {
         WIDTH = Panel_Width - (Panel_Width % 32);
+        Debug("Four Bit align");
     }
     else
     {
         WIDTH = Panel_Width;
+        Debug("no Four Bit align");
     }
     UWORD HEIGHT = Panel_Height;
 
@@ -452,54 +495,17 @@ UBYTE Display_BMP_HST(UWORD Panel_Width, UWORD Panel_Height, UDOUBLE Init_Target
         return -1;
     }
 
-    int startX = x;
-    int startY = y;
-    int sizeX = 200;
-    int sizeY = 200;
-
     Paint_NewImage(Refresh_Frame_Buf, WIDTH, HEIGHT, 0, BLACK);
     Paint_SelectImage(Refresh_Frame_Buf);
     Epd_Mode(epd_mode);
     Paint_SetBitsPerPixel(BitsPerPixel);
     // Paint_Clear(WHITE);
 
-    char Path[30];
+   
     sprintf(Path, "./pic/2.bmp");
 
-    GUI_ReadBmp(Path, 0, 0);
-
-    // you can draw your character and pattern on the image, for color definition of all BitsPerPixel, you can refer to GUI_Paint.h,
-    //   Paint_DrawRectangle(startX, startY, sizeX, sizeY, 0x30, DOT_PIXEL_3X3, DRAW_FILL_EMPTY);
-    // Paint_DrawCircle(WIDTH*3/4, HEIGHT/4, 100, 0xF0, DOT_PIXEL_2X2, DRAW_FILL_EMPTY);
-    //   Paint_DrawNum(WIDTH/4, HEIGHT/5, 709, &Font20, 0x30, 0xB0);
-
-    switch (BitsPerPixel)
-    {
-    case BitsPerPixel_8:
-    {
-        Paint_DrawString_EN(10, 10, "8 bits per pixel 16 grayscale", &Font24, 0xF0, 0x00);
-        EPD_IT8951_8bp_Refresh(Refresh_Frame_Buf, startX, startY, sizeX, sizeY, false, Init_Target_Memory_Addr);
-        break;
-    }
-    case BitsPerPixel_4:
-    {
-        Paint_DrawString_EN(10, 10, "4 bits per pixel 16 grayscale", &Font24, 0xF0, 0x00);
-        EPD_IT8951_4bp_Refresh(Refresh_Frame_Buf, startX, startY, sizeX, sizeY, false, Init_Target_Memory_Addr, false);
-        break;
-    }
-    case BitsPerPixel_2:
-    {
-        Paint_DrawString_EN(10, 10, "2 bits per pixel 4 grayscale", &Font24, 0xC0, 0x00);
-        EPD_IT8951_2bp_Refresh(Refresh_Frame_Buf, startX, startY, sizeX, sizeY, false, Init_Target_Memory_Addr, false);
-        break;
-    }
-    case BitsPerPixel_1:
-    {
-        Paint_DrawString_EN(10, 10, "1 bit per pixel 2 grayscale", &Font24, 0x80, 0x00);
-        EPD_IT8951_1bp_Refresh(Refresh_Frame_Buf, startX, startY, sizeX, sizeY, A2_Mode, Init_Target_Memory_Addr, false);
-        break;
-    }
-    }
+    GUI_ReadBmp(Path, 0, 0);   
+    EPD_IT8951_8bp_Refresh(Refresh_Frame_Buf, x, y, WIDTH, HEIGHT, false, Init_Target_Memory_Addr);
 
     if (Refresh_Frame_Buf != NULL)
     {
@@ -507,11 +513,10 @@ UBYTE Display_BMP_HST(UWORD Panel_Width, UWORD Panel_Height, UDOUBLE Init_Target
         Refresh_Frame_Buf = NULL;
     }
 
-    // DEV_Delay_ms(5000);
-
     return 0;
 }
 
+/******************************************************************************
 /******************************************************************************
 function: Dynamic_Refresh_Example
 parameter:
