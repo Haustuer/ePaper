fafc91f07177f2e32e8e2d497d730bdccb3d5016
Haustuer <54508934+Haustuer@users.noreply.github.com>
2025-02-18 20:32:59 -0800

    debug

--- lib/GUI/GUI_Paint.c
+++ lib/GUI/GUI_Paint.c
@@ -176,7 +176,7 @@ parameter:
 void Paint_SetPixel(UWORD Xpoint, UWORD Ypoint, UWORD Color)
 {
     if(Xpoint > Paint.Width || Ypoint > Paint.Height){
-        //Debug("Exceeding display boundaries\r\n");
+        Debug("Exceeding display boundaries2\r\n");
         return;
     }      
     UWORD X, Y;
