70b03d6398993146a56a2b1dd1192034a866e1b4
Haustuer <54508934+Haustuer@users.noreply.github.com>
2025-02-19 01:15:26 -0800

    Update GUI_BMPfile.c

--- lib/GUI/GUI_BMPfile.c
+++ lib/GUI/GUI_BMPfile.c
@@ -208,7 +208,7 @@ static void DrawMatrix2(UWORD Xstart, UWORD Ystart, UWORD w, UWORD h ,UWORD Widt
 	UWORD xid,yid;
 	for (yid=0,j=Ypos;yid<(h);yid++,j++)
 	{
- 		y=(Ystart+yid+4);
+ 		y=(Ystart+yid+2);
 
 		
 		for (xid=0,i=Xpos;xid<(w);xid++,i++)
