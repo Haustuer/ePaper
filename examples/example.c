201e03d06dc4d5196b4087ea2bf430e4cc61c4a0
Haustuer <54508934+Haustuer@users.noreply.github.com>
2025-02-18 23:02:05 -0800

    Teil update possible

--- examples/example.c
+++ examples/example.c
@@ -450,6 +450,8 @@ UBYTE Display_BMP_Short3( UDOUBLE Init_Target_Memory_Addr ,int x, int y, int w, 
     GUI_ReadBmp2(Path, 0, 0,800,800);   
 
 
+    
+
     EPD_IT8951_8bp_Refresh(Refresh_Frame_Buf, 0, 0, 800, 800, false, Init_Target_Memory_Addr);
 
     if (Refresh_Frame_Buf != NULL)
