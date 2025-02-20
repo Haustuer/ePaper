e74426a993b62942e729530019789178e21fd552
Haustuer <54508934+Haustuer@users.noreply.github.com>
2025-02-19 20:48:34 -0800

    gtfsju

--- examples/main.c
+++ examples/main.c
@@ -276,7 +276,7 @@ int main(int argc, char *argv[])
         break;
         case 4:
 
-                Display_Icon(Init_Target_Memory_Addr,500,1700,0);
+                Display_Icon(Init_Target_Memory_Addr,1700,500,0);
         break;
 
         case 5:
