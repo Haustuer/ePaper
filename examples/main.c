95467547be447632ca77f5ab2ed307735e826efd
Haustuer <54508934+Haustuer@users.noreply.github.com>
2025-02-19 21:01:47 -0800

    sh

--- examples/main.c
+++ examples/main.c
@@ -280,7 +280,7 @@ int main(int argc, char *argv[])
         break;
 
         case 5:
-            int y = 0;
+            int y = 20;
         for (int x = 0; x < 1200; x += 180) {
            
         Display_BMP_Patch(Init_Target_Memory_Addr, x, y , 600, 300); 
