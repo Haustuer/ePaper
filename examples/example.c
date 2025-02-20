7ea9cb76f48209e0f999f1b8ba79210f0732142d
Haustuer <54508934+Haustuer@users.noreply.github.com>
2025-02-19 19:52:03 -0800

    Update example.c

--- examples/example.c
+++ examples/example.c
@@ -518,6 +518,7 @@ UBYTE Display_Icon( UDOUBLE Init_Target_Memory_Addr ,int x, int y,  int Icon)
 
     Debug("example befor w:%d h:%d\n",w,h);
     int* mysize=GUI_ReadBmp3(Path, x, y,Icon);   
+    Debug("Function over");
     w=mysize[0];
     h=mysize[1];
     free(mysize);
