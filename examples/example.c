2e18d8f2de0e4e629630f463a51b27fc21582892
Haustuer <54508934+Haustuer@users.noreply.github.com>
2025-02-19 19:58:45 -0800

    gf

--- examples/example.c
+++ examples/example.c
@@ -517,11 +517,13 @@ UBYTE Display_Icon( UDOUBLE Init_Target_Memory_Addr ,int x, int y,  int Icon)
     int h = 2;//BigShip.height;
 
     Debug("example befor w:%d h:%d\n",w,h);
-    int* mysize=GUI_ReadBmp3(Path, x, y,Icon);   
+ //   int* mysize=GUI_ReadBmp3(Path, x, y,Icon);   
+
+    GUI_ReadBmp3(Path, x, y,Icon);   
     Debug("Function over");
-    w=mysize[0];
-    h=mysize[1];
-    free(mysize);
+ //   w=mysize[0];
+   // h=mysize[1];
+  //  free(mysize);
     Debug("Exsample After w:%d h:%d\n",w,h);
     EPD_IT8951_8bp_Refresh(Refresh_Frame_Buf, x, y, w, h, false, Init_Target_Memory_Addr);
 
